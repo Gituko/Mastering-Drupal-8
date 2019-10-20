6bb4e1e39aab8bf0d8ac29661f8870e2a5b3e253
Sammy G <mwangigituko@gmail.com>
2019-10-20 14:32:31 +0000

    -fk20 dependency injection all the way!

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -1,21 +1,47 @@
 <?php
 
-    namespace Drupal\hello_world\Controller;
+namespace Drupal\hello_world\Controller;
 
-    use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello_world\HelloWorldSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/*
+ * Controller for the salutation message
+ */
+class HelloWorldController extends ControllerBase{
+
+
+    /**
+     * @var \Drupal\hello_world\HelloWorldSalutation
+     */
+    protected $salutation;
+    /**
+     * HelloWorldController constructor.
+     *
+     * @param \Drupal\hello_world\HelloWorldSalutation $salutation
+     */
+    public function __construct(HelloWorldSalutation $salutation) {
+        $this->salutation = $salutation;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('hello_world.salutation')
+        );
+    }
+
 
     /*
-     * Controller for the salutation message
+     * Hello World
+     *
+     * @return string
      */
-    class HelloWorldController extends ControllerBase{
-        /*
-         * Hello World
-         *
-         * @return string
-         */
-        public function helloWorld(){
-            return[
-                    '#markup'=>$this->t('Hello FedoraKing!')
-                ];
-        }
+    public function helloWorld(){
+        return [
+            '#markup' => $this->salutation->getSalutation(),
+        ];
     }
+}
--- web/modules/custom/hello_world/src/HelloWorldSalutation.php
+++ web/modules/custom/hello_world/src/HelloWorldSalutation.php
@@ -4,21 +4,31 @@
 
     use Drupal\Core\StringTranslation\StringTranslationTrait;
 
-    class HelloWorldController{
+
+    /*
+     * prepares the salutation
+     */
+
+    class HelloWorldSalutation{
+
+        use StringTranslationTrait;
+
         /*
-         * prepares the salutation
+         * Returns the salutation
          */
         public function getSalutation(){
 
-            $time = new \DateTime();
-            if((int) $time->format('G') >= 06 && (int) $time->format('G') < 12 ){
-                return $this->t('Good morning world');
-            }
-            if((int) $time->format('G') >= 12 && (int) $time->format('G') < 18 ){
-                return $this->t('Good afternoon world');
-            }
-            if((int) $time->format('G') >= 18 ){
-                return $this->t('Good evening world');
-            }
+
+              $time = new \DateTime();
+              if((int) $time->format('G') >= 06 && (int) $time->format('G') < 12 ){
+                  return $this->t('Good morning world');
+              }
+              if((int) $time->format('G') >= 12 && (int) $time->format('G') < 18 ){
+                  return $this->t('Good afternoon world');
+              }
+              if((int) $time->format('G') >= 18 ){
+                  return $this->t('Good evening world');
+              }
+
          }
     }
